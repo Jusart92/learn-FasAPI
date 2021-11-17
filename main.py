7624c8808c0e68417918ee5ddf58d878cacd9714
Arturo Juarez <jusart.92@gmail.com>
2021-11-17 09:14:45 -0600

    .

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi import Body, Query
+
+app = FastAPI()
+
+# Models
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
+@app.get("/")
+def home():
+    return {"message": "Hello World!"}
+
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+):
+    return {"name": name, "age": age}
