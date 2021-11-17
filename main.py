a2a93a83a3ae828bc24f182c45116e324697be1a
Arturo Juarez <jusart.92@gmail.com>
2021-11-17 11:40:15 -0600

    example on swagger

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from enum import Enum
 # Pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr
 
 # FastAPI
 from fastapi import FastAPI
@@ -24,9 +25,21 @@ class HeirColor(Enum):
 
 
 class Location(BaseModel):
-    city: str
-    state: str
-    contry: str
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+    )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+    )
+    contry: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+    )
 
 
 class Person(BaseModel):
@@ -34,34 +47,52 @@ class Person(BaseModel):
         ...,
         min_length=1,
         max_length=50,
+        example="Arturo",
     )
     last_name: str = Field(
         ...,
         min_length=1,
         max_length=50,
+        example="Juarez",
     )
+
+    email: EmailStr = Field(..., example="jusart.92@gmail.com")
     age: int = Field(
         ...,
         gt=0,
         le=115,
+        example=25,
     )
-    hair_color: Optional[HeirColor] = Field(default=None)
+    hair_color: Optional[HeirColor] = Field(
+        default=None)
     is_married: Optional[bool] = Field(
         default=None,
     )
 
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "first_name": "John",
+    #             "last_name": "Doe",
+    #             "email": "jusart.92@gmail.com",
+    #             "age": 25,
+    #             "hair_color": "brown",
+    #             "is_married": True,
+    #         }
+    #     }
 
-@app.get("/")
+
+@ app.get("/")
 def home():
     return {"message": "Hello World!"}
 
 
-@app.post("/person/new")
+@ app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
 
 
-@app.get("/person/detail")
+@ app.get("/person/detail")
 def show_person(
     name: Optional[str] = Query(
         None, min_length=1,
@@ -80,7 +111,7 @@ def show_person(
 # Validaciones: Path Parameters
 
 
-@app.get("/person/detail/{person_id}")
+@ app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(..., gt=0)
 ):
@@ -89,7 +120,7 @@ def show_person(
 # Validaciones: Request Body
 
 
-@app.put("/person/{person_id}")
+@ app.put("/person/{person_id}")
 def update_person(
     person_id: int = Path(
         ...,
